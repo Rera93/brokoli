88535e5a15fd23460c409261f750e1a2d36e3bab
Brigel Pineti <b.pineti@student.ru.nl>
2017-10-30 11:51:16 +0100

    added index.js, projects,js and types.js to the actions folder

---
+++ brokoli/app/actions/index.js
@@ -0,0 +1,9 @@
+import * as ProjectActions from './projects';
+
+/* 
+   Grouping all the potential actions the app could perform
+   in the ActionCreators const. Useful as app begins to grow.
+*/
+export const ActionCreators = Object.assign({},
+    ProjectActions, 
+);
