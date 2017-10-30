fd6dc524f916010c5f542780ce0738872ce70098
Brigel Pineti <b.pineti@student.ru.nl>
2017-10-30 11:27:52 +0100

    created api.js and createReducer.js

---
+++ brokoli/app/lib/createReducer.js
@@ -0,0 +1,12 @@
+//This file will handle the creation of a reducer
+//handlers are the action types that are being listened
+
+export default function createReducer(initialState, handlers) {
+    return function reducer(state = initialState, action) {
+      if (handlers.hasOwnProperty(action.type)) {
+        return handlers[action.type](state, action)
+      } else {
+        return state
+      }
+    }
+  }
