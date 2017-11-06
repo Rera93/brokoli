fd1e1c051a32e76221fee2528aa10efee7a29e17
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-06 19:48:01 +0100

    Incorporated ViewContainer to the BookmarkScreen

---
+++ brokoli/app/containers/BookmarkScreen.js
@@ -0,0 +1,25 @@
+/*
+  Created by Brigel Pineti
+
+  Bookmark Screen will store projects that are of interest for the user.
+*/
+
+'use strict'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import ViewContainer from '../components/ViewContainer'
+
+class BookmarkScreen extends React.Component {
+
+    render(){
+        return(
+            <ViewContainer>
+                
+                </ViewContainer>
+
+        )
+    }
+}
+
+module.exports = BookmarkScreen
