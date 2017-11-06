b7738dfa1a8e033be59c80059fc4f3c68c1a182e
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-06 19:53:59 +0100

    Incorporated ViewContainer to the BrokoliScreen

---
+++ brokoli/app/containers/BrokoliScreen.js
@@ -0,0 +1,26 @@
+/*
+  Created by Brigel Pineti
+
+  Brokoli Screen will store a queue of projects that will be arranged depending on user's
+  profile details. The priority for projects will depend on the number of brokoli's. 
+*/
+
+'use strict'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import ViewContainer from '../components/ViewContainer'
+
+class BrokoliScreen extends React.Component {
+
+    render(){
+        return(
+            <ViewContainer>
+                
+                </ViewContainer>
+
+        )
+    }
+}
+
+module.exports = BrokoliScreen
