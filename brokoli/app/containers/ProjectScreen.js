9b66ec3f0bbeb8fef4d0a39d24c5b86eb4f3fa9a
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-06 20:00:17 +0100

    Incorporated ViewContainer to the ProjectScreen

---
+++ brokoli/app/containers/ProjectScreen.js
@@ -0,0 +1,26 @@
+/*
+  Created by Brigel Pineti
+
+  Project Screen will allow users to create a project abstract which will be added to the project queue
+  of the Brokoli Screen.
+*/
+
+'use strict'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import ViewContainer from '../components/ViewContainer'
+
+class ProjectScreen extends React.Component {
+
+    render(){
+        return(
+            <ViewContainer>
+                
+                </ViewContainer>
+
+        )
+    }
+}
+
+module.exports = ProjectScreen
