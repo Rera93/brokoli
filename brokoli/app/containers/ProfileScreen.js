09fcd59bf71bd2dc264b5e7f185d326368b59753
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-06 19:57:58 +0100

    Incorporated ViewContainer to the ProfileScreen

---
+++ brokoli/app/containers/ProfileScreen.js
@@ -0,0 +1,25 @@
+/*
+  Created by Brigel Pineti
+
+  Profile Screen will contain user details.
+*/
+
+'use strict'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import ViewContainer from '../components/ViewContainer'
+
+class ProfileScreen extends React.Component {
+
+    render(){
+        return(
+            <ViewContainer>
+                
+                </ViewContainer>
+
+        )
+    }
+}
+
+module.exports = ProfileScreen
