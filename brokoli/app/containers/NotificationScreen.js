dac3f159c57b17030c30ebb6c741325511567238
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-06 19:56:39 +0100

    Incorporated ViewContainer to the NotificationScreen

---
+++ brokoli/app/containers/NotificationScreen.js
@@ -0,0 +1,26 @@
+/*
+  Created by Brigel Pineti
+
+  Notification Screen will store notifications for users who have applied for a position 
+  and for users you have posted a project abstract. 
+*/
+
+'use strict'
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import ViewContainer from '../components/ViewContainer'
+
+class NotificationScreen extends React.Component {
+
+    render(){
+        return(
+            <ViewContainer>
+                
+                </ViewContainer>
+
+        )
+    }
+}
+
+module.exports = NotificationScreen
