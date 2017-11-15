197a29cb9e86e735e72b12f3e99b1653633587d1
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-15 16:14:44 +0100

    Added Logo and improved the structure of components

--- brokoli/App.js
+++ brokoli/App.js
@@ -18,7 +18,7 @@ import ProjectScreen from './app/containers/ProjectScreen'
 import BookmarkScreen from './app/containers/BookmarkScreen'
 import BrokoliScreen from './app/containers/BrokoliScreen'
 import ProfileScreen from './app/containers/ProfileScreen'
-import NotificationScreen from './app/containers/NotificationScreen'
+import NotificationScreen from './app/containers/LoginScreen'
 
 //middleware that logs actions
 const loggerMiddleware = createLogger({ predicate : (getState, action) => __DEV__});
@@ -76,7 +76,7 @@ export default class App extends React.Component {
 
                <Scene key="brokoliTab" 
                       title="Brokoli"
-                      source={require('./img/icons/broccoli.png')}
+                      source={require('./img/icons/brokoli.png')}
                       icon={TabIcon}
                       component={BrokoliScreen}
                       hideNavBar />
---
+++ brokoli/app/containers/RegisterScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+
+import ViewContainer from '../components/ViewContainer'
+import FullName from '../components/RegisterScreen/FullName'
+import Email from '../components/RegisterScreen/Email'
+import Username from '../components/RegisterScreen/Username'
+import Password from '../components/RegisterScreen/Password'
+
+export default class LoginScreen extends React.Component {
+
+
+    render(){
+
+        return(
+
+            <ViewContainer>
+
+              {/* TODO: Stack Navigator */}  
+
+                </ViewContainer>
+
+        )
+    }
+}
