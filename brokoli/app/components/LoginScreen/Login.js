637aa6d80b9d4ec25eadd679909d767d58c02b5c
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-15 13:23:19 +0100

    Added Login Component and placed it into the LoginScreen container

---
+++ brokoli/app/components/LoginScreen/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Button,
+  Alert,
+  Image
+} from 'react-native';
+
+export default class Login extends React.Component {
+
+    render(){
+        return(
+            <Text> Login </Text>
+        )
+    }
+}
