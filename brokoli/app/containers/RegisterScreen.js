fd1e0ce263d100daf28d564fd5328493bd8fc6fb
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-18 22:45:50 +0100

    imported library to configure Stack Nav. Replace fade down with slide to left/right

--- brokoli/app/containers/RegisterScreen.js
+++ brokoli/app/containers/RegisterScreen.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+import getSlideFromRightTransition from 'react-navigation-slide-from-right-transition';
 
 import ViewContainer from '../components/ViewContainer'
-
 import Personal from '../components/RegisterScreen/Personal/Personal'
 import Account from '../components/RegisterScreen/Account/Account'
 import Professional from '../components/RegisterScreen/Professional/Professional'
 import Career from '../components/RegisterScreen/Career/Career'
 import Additional from '../components/RegisterScreen/Additional/Additional'
-
-
 
 const StackNav = StackNavigator({
 
@@ -20,6 +17,8 @@ const StackNav = StackNavigator({
     Professional: {screen: Professional},
     Career: {screen: Career},
     Additional: {screen: Additional}
+}, {
+    transitionConfig: getSlideFromRightTransition
 });
 
 export default class RegisterScreen extends React.Component {
