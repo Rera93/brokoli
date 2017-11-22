c9c561f7887c0d2cafc08a7bc682a4b70bf4099a
Brigel Pineti <b.pineti@student.ru.nl>
2017-11-22 17:07:09 +0100

    Config file for swipe added to lib folder. Swipe.js in Brokoli is importing the swipe file from lib

--- brokoli/app/components/BrokoliScreen/Swipe.js
+++ brokoli/app/components/BrokoliScreen/Swipe.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 
-import SwipeCards from 'react-native-swipe-cards';
+import SwipeCards from '../../lib/SwipeCards'
 
 class Card extends React.Component {
     constructor(props) {
