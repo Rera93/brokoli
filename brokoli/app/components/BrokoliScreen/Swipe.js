272cdec67f914b11159ca1207c5df4552ac4fe33
Alan R <alanribeiroandrade@gmail.com>
2017-11-22 18:17:33 +0100

    retrieval and swipe of projects

--- brokoli/app/components/BrokoliScreen/Swipe.js
+++ brokoli/app/components/BrokoliScreen/Swipe.js
@@ -3,61 +3,92 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 
 import SwipeCards from 'react-native-swipe-cards';
 
-class Card extends React.Component {
+class Project extends React.Component {
+    constructor(props) {
+      super(props);
+      
+    }
+  
+     render() {
+      return (
+        <View style={[styles.card]}>
+          <Text style={styles.projectTitle}>{this.props.doc.name}</Text>
+          <Text style={styles.text}>{this.props.doc.description}</Text>
+          <Text style={styles.text}>Position(s) available in this project: {"\n"}
+                    {this.props.doc.positions}</Text>
+        </View>
+      )
+    }
+  }
+  
+  class NoMoreProjects extends React.Component {
     constructor(props) {
       super(props);
     }
   
     render() {
       return (
-        <View style={styles.card}>
-          <Image style={styles.thumbnail} source={{uri: this.props.image}} />
-          <Text style={styles.text}>This is card {this.props.name}</Text>
+        <View style={styles.noMoreProjects}>
+          <Text>No more projects</Text>
         </View>
       )
     }
   }
   
-  class NoMoreCards extends React.Component {
-    constructor(props) {
-      super(props);
-    }
+  // var cards = [
+  //   {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  //   {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  //   {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  //   {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  //   {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  //   {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  //   {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  //   {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  //   {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+  // ]
   
-    render() {
-      return (
-        <View style={styles.noMoreCards}>
-          <Text>No more cards</Text>
-        </View>
-      )
-    }
-  }
-  
-  const cards = [
-    {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
-    {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
-    {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
-    {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
-    {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
-    {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
-    {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
-    {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
-    {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
-  ]
-  
-  const cards2 = [
-    {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
-    {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
-    {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
-    {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
-  ]
+  // const cards2 = [
+  //   {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  //   {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  //   {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  //   {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+  // ]
   
   export default class Swipe extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        cards: cards,
-        outOfCards: false
+        projects: [],
+        outOfProjects: false
       }
+    }
+
+    componentDidMount(){
+      this.getData();
+    }
+
+    getData(){
+      var arrayProjects = [];
+      return fetch('https://brokoli.eu-gb.mybluemix.net/api/visitors', {  
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+        // .then(function(response) { return response.json(); })
+        // .then(function(responseData) {
+        //   this.setState({ data : responseData})});
+
+        .then((response) => response.json())
+        .then((responseData) => {
+          for (var i = 0; i < responseData.length; i++) {
+            var object = responseData[i];
+            arrayProjects.push(object);
+          }
+          this.setState({projects: arrayProjects})
+        });
+
     }
   
     handleYup (card) {
@@ -68,41 +99,18 @@ class Card extends React.Component {
       console.log("nope")
     }
   
-    cardRemoved (index) {
-      console.log(`The index is ${index}`);
-  
-      let CARD_REFRESH_LIMIT = 3
-  
-      if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
-        console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
-  
-        if (!this.state.outOfCards) {
-          console.log(`Adding ${cards2.length} more cards`)
-  
-          this.setState({
-            cards: this.state.cards.concat(cards2),
-            outOfCards: true
-          })
-        }
-  
-      }
-  
-    }
-  
+    
     render() {
       return (
         <SwipeCards
-          cards={this.state.cards}
-          loop={false}
-  
-          renderCard={(cardData) => <Card {...cardData} />}
-          renderNoMoreCards={() => <NoMoreCards />}
-          showYup={true}
-          showNope={true}
-  
+          cards={this.state.projects}
+          renderCard={(projectData) => <Project {...projectData} />}
+          renderNoMoreProjects={() => <NoMoreProjects />}
+
           handleYup={this.handleYup}
           handleNope={this.handleNope}
-          cardRemoved={this.cardRemoved.bind(this)}
+          handleMaybe={this.handleMaybe}
+          hasMaybeAction
         />
       )
     }
@@ -110,6 +118,7 @@ class Card extends React.Component {
   
   const styles = StyleSheet.create({
     card: {
+      flex: 1,
       alignItems: 'center',
       borderRadius: 5,
       overflow: 'hidden',
@@ -117,6 +126,8 @@ class Card extends React.Component {
       backgroundColor: 'white',
       borderWidth: 1,
       elevation: 1,
+      marginTop :30,
+      marginBottom: 10,
     },
     thumbnail: {
       width: 300,
@@ -127,7 +138,12 @@ class Card extends React.Component {
       paddingTop: 10,
       paddingBottom: 10
     },
-    noMoreCards: {
+    projectTitle: {
+      fontSize: 40,
+      paddingTop: 10,
+      paddingBottom: 10
+    },
+    noMoreProjects: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
