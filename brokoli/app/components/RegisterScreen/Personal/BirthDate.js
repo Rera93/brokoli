2ad94e28bf0065c3e32fc35ec08af44fd3e2555f
Brigel Pineti <b.pineti@student.ru.nl>
2017-12-03 10:11:22 +0100

    created date picker for birth date. Need to fetch data from date picker.

--- brokoli/app/components/RegisterScreen/Personal/BirthDate.js
+++ brokoli/app/components/RegisterScreen/Personal/BirthDate.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
+import DatePicker from 'react-native-modal-datetime-picker'
 
 import ViewContainer from '../../ViewContainer'
 
 export default class BirthDate extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+                        isDateTimePickerVisible: false,
+                        text: 'yyyy-mm-dd',
+                     };
 
+    }
+   
+
+    _showDatePicker = () => this.setState({isDateTimePickerVisible: true});
+
+    _hideDatePicker = () => this.setState({isDateTimePickerVisible: false});
+
+    _handleDatePicked = (date) => {
+        console.log('Picked date: ', date);
+        this.setState({text: date})
+        this._hideDatePicker();
+    };
+    
     render(){
         
 
         return(
 
             <ViewContainer>
+                
+              <TouchableOpacity onPress={this._showDatePicker}>
+              <Text> {this.state.text} </Text>
+              </TouchableOpacity> 
 
-              <Text> Hello, birth date. </Text>
+              <DatePicker 
+                isVisible = {this.state.isDateTimePickerVisible}
+                onConfirm = {this._handleDatePicked}
+                onCancel = {this._hideDatePicker} />
         
                 </ViewContainer>
 
