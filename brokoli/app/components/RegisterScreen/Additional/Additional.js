3dfa6e402e3934b3785942144bc415e6f78c6978
Brigel Pineti <b.pineti@student.ru.nl>
2017-12-16 13:07:45 +0100

    separate skills from additional. skills are now contained into a file called skill.js which is imported in additional.js

--- brokoli/app/components/RegisterScreen/Additional/Additional.js
+++ brokoli/app/components/RegisterScreen/Additional/Additional.js
@@ -22,57 +22,13 @@ export default class Additional extends React.Component {
         super(props)
 
         this.state={
-            addNew: false,
-            totalNrSkills: nrOfRequiredSkills,
-            skills: Array(nrOfRequiredSkills).fill(''),
         }
-    }
-
-    _renderSkills(){
-        var rows = []
-        for(let i=0; i < nrOfRequiredSkills; i++)
-        {
-            rows.push(
-
-                <View key = {i}
-                      style={styles.skillCont}>
-
-                    <TextInput style={styles.skill} 
-                               placeholder={'skill ' + (i+1)}
-                               onChangeText={(text) => this._grabSkill(text, i)}/>
-
-                </View>
-            )
-        }
-        console.log(this.state.skills)
-        return(
-            <View style={styles.reqSkills}>
-                {rows}
-                </View>
-        )
-    }
-
-    _grabSkill = (text, i) => {
-
-        this.state.skills[i] = text
-
-        this.setState({skills: this.state.skills})
-
-        console.log(this.state.skills)
-
     }
 
 
 
     
-    _flip(){
-        console.log("addNew: ", this.state.addNew)
-        this.setState({addNew: !this.state.addNew})
-        console.log("addNew: ", this.state.addNew)
-        
-
-        {this._addNew}      
-    }
+    
 
     _addNew(){
 
@@ -99,35 +55,21 @@ export default class Additional extends React.Component {
 
                 <Header />
 
-                <View style={styles.titleCont}>
-
-                    <Text style={styles.title}> What do you know? List at least five skills </Text>
-
-                </View>
-
-
-                 {this._renderSkills()}
-
-
-                <TouchableOpacity style={styles.add} onPress={this._flip.bind(this)}>
-
-                 <Text style={styles.plus}> + </Text>
-
-                </TouchableOpacity> 
-
-                {this._addNew()}
-
                 <Skills />
 
                 <TouchableOpacity style={styles.button}>
 
-                <Text style={styles.btnText}> Proceed </Text>
+                <Text style={styles.btnText}> PROCEED </Text>
 
                 </TouchableOpacity>
 
                 </ScrollView>
 
                 <View style={{ height: 80 }} />
+
+                
+
+                
                 </KeyboardAvoidingView>
 
         )
@@ -140,63 +82,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    titleCont: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-        marginBottom: 10
-    },
-    title: {
-        fontSize: 16,
-        color: 'grey',
-        textAlign: 'left',
-        fontWeight: '600'
-        
-    },
     button: {
     backgroundColor: '#42D260',
     marginBottom: 20,
     borderRadius: 10,
-    width: width - 100,
+    width: width - 300,
     alignItems: 'center'
     },
     btnText: {
     color : 'white',
     padding: 10,
     fontWeight: 'bold'
-}, 
-    add: {
-        backgroundColor: 'white', 
-    },
-    plus: {
-        color: '#42D260',
-        padding: 10,
-        fontSize: 55
-    },
-    skill: {
-        flex: 1,
-        width: width - 60,
-        padding: 10,
-        color: '#42D260'
-    },
-    skillCont: {
-        flex: 1,
-        borderWidth: 1,
-        borderRadius: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 5,
-        marginBottom: 5,
-        borderColor: 'grey'
-
-    },
-    reqSkills: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 10,
-    }
+},
 
 })
 
--- brokoli/app/components/RegisterScreen/Additional/Skills.js
+++ brokoli/app/components/RegisterScreen/Additional/Skills.js
@@ -1,10 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Dimensions, TextInput } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import ViewContainer from '../../ViewContainer'
 
+const width = Dimensions.get('window').width
+const nrOfRequiredSkills = 5;
+
 export default class Skills extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            addNew: false,
+            totalNrSkills: nrOfRequiredSkills,
+            skills: Array(nrOfRequiredSkills).fill(''),
+        }
+    }
+
+    _renderSkills(){
+        var rows = []
+        for(let i=0; i < nrOfRequiredSkills; i++)
+        {
+            rows.push(
+
+                <View key = {i}
+                      style={styles.skillCont}>
+
+                    <TextInput style={styles.skill} 
+                               placeholder={'skill ' + (i+1)}
+                               onChangeText={(text) => this._grabSkill(text, i)}/>
+
+                </View>
+            )
+        }
+        console.log(this.state.skills)
+        return(
+            <View style={styles.reqSkills}>
+                {rows}
+                </View>
+        )
+    }
+
+    _grabSkill = (text, i) => {
+
+        this.state.skills[i] = text
+
+        this.setState({skills: this.state.skills})
+
+        console.log(this.state.skills)
+
+    }
+
+    _flip(){
+        console.log("addNew: ", this.state.addNew)
+        this.setState({addNew: !this.state.addNew})
+        console.log("addNew: ", this.state.addNew)
+        
+
+        {this._addNew}      
+    }
+
+    _addNew(){
+        
+                if(this.state.addNew == true){
+                    return(
+                        <View style={styles.skillCont}>
+                        
+                            <TextInput style={styles.skill} placeholder='skill'/>
+                        
+                        </View> )
+                     //   this.setState({addNew: !this.state.addNew})
+                        
+                }
+            }
+        
 
     
     render(){
@@ -13,11 +84,76 @@ export default class Skills extends React.Component {
 
             <ViewContainer>
 
-              <Text> Hello, summary </Text>
+                <View style={styles.titleCont}>
+
+                    <Text style={styles.title}> What do you know? List at least five skills </Text>
+
+                </View>
+
+
+                 {this._renderSkills()}
+
+
+                <TouchableOpacity style={styles.add} onPress={this._flip.bind(this)}>
+
+                 <Text style={styles.plus}> + </Text>
+
+                </TouchableOpacity> 
+
+                {this._addNew()}
 
                 </ViewContainer>
 
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+titleCont: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 10
+},
+title: {
+    fontSize: 16,
+    color: 'grey',
+    textAlign: 'left',
+    fontWeight: '600'
+    
+},
+add: {
+    backgroundColor: 'white', 
+},
+plus: {
+    color: '#42D260',
+    padding: 10,
+    fontSize: 55
+},
+skill: {
+    flex: 1,
+    width: width - 60,
+    padding: 10,
+    color: '#42D260'
+},
+skillCont: {
+    flex: 1,
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+    marginBottom: 5,
+    borderColor: 'grey'
+
+},
+reqSkills: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+}
+});
 
