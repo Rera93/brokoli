1dac61234639ab29ccf1cff12f7fc3f5cb4c294b
Brigel Pineti <b.pineti@student.ru.nl>
2017-12-17 11:22:06 +0100

    able to fetch the state of firstName, lastName, city and country

--- brokoli/app/components/RegisterScreen/Personal/Personal.js
+++ brokoli/app/components/RegisterScreen/Personal/Personal.js
@@ -23,7 +23,69 @@ export default class Personal extends React.Component {
         headerStyle: { backgroundColor: '#42D260', marginTop: 24 },
         headerTitleStyle: { color: 'white' },
       };
+
+
+
+      constructor(props){
+          super(props)
+
+          this.state = {
+              firstName: '',
+              lastName: '',
+              dateOfBirth: '',
+              profilePic: '',
+              gender: '',
+              city: '',
+              country: '',
+          }
+      }
+
+    _grabFirstName = (text) => {
+        this.state.firstName = text 
+        this.setState(function(prevState, props){
+            return {firstName: prevState.firstName}
+         });
+
+         console.log('First Name: ', this.state.firstName)
+
+    }
+
+    _grabLastName = (text) => {
+
+        this.state.lastName = text 
+        this.setState(function(prevState, props){
+            return {lastName: prevState.lastName}
+         });
+
+         console.log('Last Name: ', this.state.lastName)
+
+        
+    }
     
+
+    _grabCity = (text) => {
+
+        this.state.city = text 
+        this.setState(function(prevState, props){
+            return {city: prevState.city}
+         });
+
+         console.log('City: ', this.state.city)
+
+        
+    }
+
+    _grabCountry = (text) => {
+
+        this.state.country = text 
+        this.setState(function(prevState, props){
+            return {country: prevState.country}
+         });
+
+         console.log('Country: ', this.state.country)
+
+        
+    }
     render(){
 
         const {navigate}= this.props.navigation
@@ -45,9 +107,13 @@ export default class Personal extends React.Component {
                 
                 <View style={styles.inputCont}> 
                     <Text style={[styles.cond, {color: '#BE2625'}]}> R </Text>
-                    <TextInput style={styles.locInput} placeholder='first name' />
+                    <TextInput style={styles.locInput} 
+                               placeholder='first name'
+                               onChangeText={(text) => this._grabFirstName(text)}/>
                     <Text style={[styles.cond, {color: '#BE2625'}]}> R </Text>
-                    <TextInput style={styles.locInput} placeholder='last name'/>  
+                    <TextInput style={styles.locInput} 
+                               placeholder='last name'
+                               onChangeText={(text) => this._grabLastName(text)}/>  
                 </View>
                 <ProfilePic />
                 <BirthDate />
@@ -55,9 +121,13 @@ export default class Personal extends React.Component {
 
                 <View style={styles.inputCont}> 
                     <Text style={[styles.cond, {color: '#BE2625'}]}> R </Text>
-                    <TextInput style={styles.locInput} placeholder='city' />
+                    <TextInput style={styles.locInput}
+                               placeholder='city'
+                               onChangeText={(text) => this._grabCity(text)} />
                     <Text style={[styles.cond, {color: '#BE2625'}]}> R </Text>
-                    <TextInput style={styles.locInput} placeholder='country'/>  
+                    <TextInput style={styles.locInput} 
+                               placeholder='country'
+                               onChangeText={(text) => this._grabCountry(text)}/>  
                 </View>
 
                 <TouchableOpacity onPress={()=> navigate('Account')}
