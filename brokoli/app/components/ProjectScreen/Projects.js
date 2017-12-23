55ce39f00fdae7442fd35824f6130f36f83cf8d1
Brigel Pineti <b.pineti@student.ru.nl>
2017-12-23 22:26:57 +0100

    Updated design of your projects screen

--- brokoli/app/components/ProjectScreen/Projects.js
+++ brokoli/app/components/ProjectScreen/Projects.js
@@ -8,7 +8,7 @@ import { StyleSheet,
          ScrollView, 
          View,
          FlatList,
-        TouchableOpacity} from 'react-native';
+        TouchableOpacity, Image} from 'react-native';
 
 
 
@@ -37,7 +37,9 @@ export default class Projects extends React.Component {
      componentDidMount() {
         this.makeRemoteRequest();
       }
-    
+
+      //This is just an example where a bunch of data is being fetched from the API below. 
+      //Need to fetch data from db and display them in the flat list.
       makeRemoteRequest = () => {
         const { page, seed } = this.state;
         const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
@@ -71,11 +73,26 @@ export default class Projects extends React.Component {
           <FlatList
             data={this.state.data}
             renderItem={({ item }) => (
-              <View>
-                <Text> {`${item.name.first} ${item.name.last}`} </Text>
-                <Text> {item.email} </Text>
-              </View>
+              <TouchableOpacity style={styles.item}>
+                <View style={styles.nameCont}>
+                {/*Name of project*/}
+                <Text style={styles.name}> {item.email} </Text>
+                </View>
+                <View style={styles.reactionsCont}>
+                <View style={styles.reactions}>
+                {/*Number of applicants until now*/}
+                <Image style={styles.icon} source={require('../../../img/icons/applicants.png')} />
+                <Text> {item.name.first}  </Text>
+                </View>
+                <View  style={styles.reactions}>
+                 {/*Number of brokoli's until now*/}
+                 <Image style={styles.icon} source={require('../../../img/icons/brokolis.png')} />
+                 <Text> {item.name.last}  </Text>
+                 </View>
+                </View>
+              </TouchableOpacity>
             )}
+            keyExtractor={item => item.email}
           />
         </ScrollView>
        
@@ -87,5 +104,33 @@ export default class Projects extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  item:{
+    backgroundColor: 'white',
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 20,
+    paddingTop: 20
+  },
+  reactionsCont: {
+    flexDirection: 'row'
+  },
+  reactions: {
+    flexDirection: 'row'
+  },
+  icon: {
+    resizeMode: 'contain',
+    width: 25,
+    height: 25,
+    tintColor: '#254D32'
+  },
+  nameCont: {
+
+
+
+  }
+
 });
               
