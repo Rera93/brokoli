3e02c385935df427dc9ffcde31483b65db494350
Brigel Pineti <b.pineti@student.ru.nl>
2017-12-28 15:56:30 +0100

    moved tab nav to HomeScreen.js container

--- brokoli/App.js
+++ brokoli/App.js
@@ -12,19 +12,14 @@ import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
 import reducer from './app/reducers'
 import { Router, Scene, Actions, ActionConst, Stack } from 'react-native-router-flux'
-import { TabNavigator } from 'react-navigation';
 import { StackNavigator } from 'react-navigation';
 import getSlideFromRightTransition from 'react-navigation-slide-from-right-transition';
 
 
 import AppContainer from './app/containers/AppContainer'
-import ProjectScreen from './app/containers/ProjectScreen'
-import BookmarkScreen from './app/containers/BookmarkScreen'
-import BrokoliScreen from './app/containers/BrokoliScreen'
-import ProfileScreen from './app/containers/ProfileScreen'
-import NotificationScreen from './app/containers/NotificationScreen'
 import LoginScreen from './app/containers/LoginScreen'
 import RegisterScreen from './app/containers/RegisterScreen'
+import HomeScreen from './app/containers/HomeScreen'
 
 
 //middleware that logs actions
@@ -42,52 +37,13 @@ function configureStore(initialState) {
 
 const store = configureStore({});
 
-const TabNav =  TabNavigator({
-  Project: {
-    screen: ProjectScreen,
-  },
-  Bookmark: {
-    screen: BookmarkScreen,
-  },
-  Brokoli: {
-    screen: BrokoliScreen,
-  },
-  Profile: {
-    screen: ProfileScreen,
-  },
-  Mail: {
-    screen: NotificationScreen,
-  },
-}, {
-  initialRouteName: 'Brokoli',
-  tabBarPosition: 'bottom',
-  animationEnabled: true,
-  swipeEnabled: false,
-  tabBarOptions: {
-    activeTintColor: '#42D260',
-    showIcon: true,
-    labelStyle: {
-      fontSize: 9,
-      fontWeight: 'bold'
-    },
-    style: {
-      borderTopWidth: 1,
-      borderColor: '#42D260',
-      backgroundColor: 'white',
-    },
-    inactiveTintColor: 'grey',
-    indicatorStyle: {
-      backgroundColor: '#42D260',
-    }
-  
-  },
-});
+
 
 const StackNav = StackNavigator({
   
       Login: {screen: LoginScreen},
       Register: {screen: RegisterScreen},
-      Home: {screen : TabNav}
+      Home: {screen : HomeScreen}
   }, { headerMode: 'none',
        transitionConfig: getSlideFromRightTransition
       
@@ -110,19 +66,7 @@ export default class App extends React.Component {
   }
 }
 const styles = StyleSheet.create({
-customizeFooter: {
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'white',
-  borderTopWidth: 0.7,
-  borderColor: '#228B22',
-},
-icon:{
-  height: 30,
-  width: 30,
-  resizeMode: 'contain'
-}
+
 });
 
 
---
+++ brokoli/app/containers/HomeScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import ProjectScreen from '../containers/ProjectScreen'
+import BookmarkScreen from '../containers/BookmarkScreen'
+import BrokoliScreen from '../containers/BrokoliScreen'
+import ProfileScreen from '../containers/ProfileScreen'
+import NotificationScreen from '../containers/NotificationScreen'
+
+const TabNav =  TabNavigator({
+    
+      
+      Project: {
+        screen: ProjectScreen,
+      },
+      Bookmark: {
+        screen: BookmarkScreen,
+      },
+      Brokoli: {
+        screen: BrokoliScreen,
+      },
+      Profile: {
+        screen: ProfileScreen,
+      },
+      Mail: {
+        screen: NotificationScreen,
+      },
+    }, {
+      initialRouteName: 'Brokoli',
+      tabBarPosition: 'bottom',
+      animationEnabled: true,
+      swipeEnabled: false,
+      tabBarOptions: {
+        activeTintColor: '#42D260',
+        showIcon: true,
+        labelStyle: {
+          fontSize: 9,
+          fontWeight: 'bold'
+        },
+        style: {
+          borderTopWidth: 1,
+          borderColor: '#42D260',
+          backgroundColor: 'white',
+        },
+        inactiveTintColor: 'grey',
+        indicatorStyle: {
+          backgroundColor: '#42D260',
+        }
+      
+      },
+    });
+
+    export default class HomeScreen extends React.Component {
+
+        render(){
+            return (
+                <TabNav />
+            )
+        }
+
+    } 
+
+    const styles = StyleSheet.create({
+        customizeFooter: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            borderTopWidth: 0.7,
+            borderColor: '#228B22',
+          },
+          icon:{
+            height: 30,
+            width: 30,
+            resizeMode: 'contain'
+          }
+    })
