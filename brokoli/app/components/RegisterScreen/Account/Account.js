b4d876c879ab85bcc9f833e0416bc2aec9ba942c
Brigel Pineti <b.pineti@student.ru.nl>
2018-01-05 19:55:40 +0100

    changed design of account

--- brokoli/app/components/RegisterScreen/Account/Account.js
+++ brokoli/app/components/RegisterScreen/Account/Account.js
@@ -10,12 +10,13 @@ import { Dimensions,
 import { StackNavigator } from 'react-navigation';
 
 const width = Dimensions.get('window').width
+const height = Dimensions
 
 export default class Account extends React.Component {
     static navigationOptions = {
         title: 'Account',
-        headerStyle: { backgroundColor: '#42D260', marginTop: 24 },
-        headerTitleStyle: { color: 'white' },
+        headerStyle: { backgroundColor: 'white', marginTop: 24 },
+        headerTitleStyle: { color: '#42D260' },
       };
 
       constructor(props) {
@@ -80,7 +81,7 @@ export default class Account extends React.Component {
 
       _flipNext(){
         if(this.state.email != '' && this.state.password != '' &&
-            this.state.passwordConf != '')
+            this.state.passwordConf != '' && this.state.username != '')
             {
                 this.state.flip = true
                 this.setState(function(prevState, props){
@@ -116,62 +117,92 @@ export default class Account extends React.Component {
 
 
             
-          <KeyboardAvoidingView behavior="padding">
+          <KeyboardAvoidingView behavior="padding" backgroundColor='#42D260'>
           
                       <ScrollView contentContainerStyle={styles.account}
-                                  showsVerticalScrollIndicator={false}>
+                                  showsVerticalScrollIndicator={false}
+                                  centerContent = {true}>
+
+                        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
 
                         <View style={styles.titleContainer}> 
 
-                          <Text style={styles.title}> Please enter your Brokoli credentials </Text>
+                          <Text style={styles.title}>Please enter your credentials</Text>
 
                           </View>
 
                         <View style={styles.inputContainer}> 
-                        <View style={[styles.condCont, {backgroundColor: '#EEAD0E'}]}>
-                          <Text style={styles.cond}> O </Text>
-                          </View>
+                    
+                          <View style={{flex: 1}}> 
                           <TextInput style={styles.input} 
                                      placeholder='username'
+                                     placeholderTextColor= 'white'
+                                     underlineColorAndroid = 'transparent'
                                      onChangeText={(text) => this._grabUsername(text)}/>
+                          </View>
+                          <View style={styles.condCont}>
+                          <Text style={styles.cond}> R </Text>
+                          </View>
                         </View>
 
                         <View style={styles.inputContainer}> 
-                         <View style={[styles.condCont, {backgroundColor: '#BE2625'}]}>
-                          <Text style={styles.cond}> R </Text>
-                          </View>
+              
+                          <View style={{flex: 1}}>
                           <TextInput style={styles.input} 
                                      placeholder='email'
+                                     placeholderTextColor= 'white'
+                                     underlineColorAndroid = 'transparent'
                                      onChangeText={(text) => this._grabEmail(text)} />
+                          </View>
+
+                          <View style={styles.condCont}>
+                          <Text style={styles.cond}> R </Text>
+                          </View>
+
                         </View>
 
                         <View style={styles.inputContainer}> 
-                          <View style={[styles.condCont, {backgroundColor: '#BE2625'} ]}>
-                          <Text style={styles.cond}> R </Text>
-                          </View>
+                          <View style={{flex: 1}}>
                           <TextInput style={styles.input} 
                                      placeholder='password'
+                                     placeholderTextColor= 'white'
+                                     underlineColorAndroid = 'transparent'
                                      onChangeText={(text) => this._grabPassword(text)} />
+                          </View>
+                          <View style={styles.condCont}>
+                          <Text style={styles.cond}> R </Text>
+                          </View>
                         </View>
 
                         <View style={styles.inputContainer}>
-                          <View style={[styles.condCont, {backgroundColor: '#BE2625'} ]}>
-                          <Text style={styles.cond}> R </Text> 
-                          </View>
+                          
+                          <View style={{flex: 1}}>
                           <TextInput style={styles.input} 
                                      placeholder='confirm password'
+                                     placeholderTextColor= 'white'
+                                     underlineColorAndroid = 'transparent'
                                      onChangeText={(text) => this._grabPasswordConf(text)} />
+                          </View>
+
+                          <View style={styles.condCont}>
+                          <Text style={styles.cond}> R </Text> 
+                          </View>
                         </View>
 
-                    <TouchableHighlight disabled={this.state.flip ? false : true} 
+                    <TouchableHighlight //disabled={this.state.flip ? false : true} 
                                         onPress={()=> navigate('Professional', {firstName: this.state.firstName, lastName: this.state.lastName, dateOfBirth: this.state.dateOfBirth, gender: this.state.gender, city: this.state.city,  country: this.state.country, username: this.state.username, email: this.state.email, password: this.state.password})}
-                                        style={[styles.button, {backgroundColor: this.state.flip ? '#42D260' : 'white'}]}>
+                                        style={[styles.button, {backgroundColor: this.state.flip ? 'white' : '#42D260'}]}>
 
-                    <Text style={[styles.btnText, {color: this.state.flip ? 'white' : '#42D260'}]}> NEXT </Text>
+                    <Text style={[styles.btnText, {color: this.state.flip ? '#42D260' : 'white'}]}> NEXT </Text>
 
                     </TouchableHighlight>
 
+                    </View>
+
                     </ScrollView>
+
+                
+                  
 
                     <View style={{height: 80}} />
 
@@ -182,11 +213,11 @@ export default class Account extends React.Component {
 }
 
 const styles = StyleSheet.create({
-	account: {
+	account: { borderWidth: 1,
             alignItems: 'center',
             justifyContent: 'center',
-            backgroundColor: 'white',
-            paddingVertical: 30
+            backgroundColor: '#42D260',
+            paddingVertical: 20,
     },
     titleContainer: {
         flex: 1,
@@ -194,46 +225,45 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         marginBottom: 15,
         marginTop: 15,
-        width: width,
     },
     title: {
-      color: 'grey',
+      color: 'white',
       fontWeight: '900',
       padding: 10,
       width: width - 100,
-      fontSize: 17,
+      fontSize: 20,
     },
     inputContainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        width: width,
-        flexDirection: 'row'
+        padding: 10,
+        flexDirection: 'row',
+        borderRadius: 5,
+        marginTop: 15,
+        marginBottom: 15,
+        marginRight: 20,
+        marginLeft: 20,
+        borderWidth: 3,
+        borderColor: 'white',
     },
     input: {
-      color: '#42D260',
-      padding: 10,
-      width: width - 100,
-      borderRadius: 5,
-      marginTop: 15,
-      marginBottom: 15,
-      marginRight: 20,
-      borderWidth: 1,
-      borderColor: 'grey',
+      color: 'white',
+      fontSize: 17,
     },
     cond: {
         fontSize: 18.5,
         textAlign: 'center',
         fontWeight: '600',
-        color: 'white'
+        color: '#42D260'
     },
     condCont: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: 27.5,
-        height: 27.5,
-        borderRadius: 30/2,
-        marginRight: 10,
+        width: 22,
+        height: 22,
+        borderRadius: 11,
+        backgroundColor: 'white'
     },
     button: {
             width: width - 300,
@@ -241,8 +271,8 @@ const styles = StyleSheet.create({
             marginBottom: 40,
             borderRadius: 10,
             alignItems: 'center',
-            borderWidth: 1,
-            borderColor: '#42D260'
+            borderWidth: 3,
+            borderColor: 'white'
     },
     btnText: {
       padding: 10,
