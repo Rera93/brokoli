ac4f2b5f73a77b186cb35362d05866e7a4f4bd02
Brigel Pineti <b.pineti@student.ru.nl>
2018-01-06 22:12:24 +0100

    final update on header

--- brokoli/app/components/RegisterScreen/Additional/Header.js
+++ brokoli/app/components/RegisterScreen/Additional/Header.js
@@ -51,15 +51,23 @@ export default class Header extends React.Component {
 
               <View style={styles.inputCont}>
 
+              <View style={{flex: 1}}>
+
               <TextInput placeholderTextColor='#C7C7CD'
                          placeholder='Header'
                          underlineColorAndroid = 'transparent'
                          multiline = {true}
-                         maxLength={120}
+                         maxLength={100}
                          onChangeText={(text) => this._grabHeader(text)}
                          value={this.state.headerContent}
                          style = {[styles.input, {height: Math.max(40, this.state.height)}]}
                          onContentSizeChange = {(event) => this._autoExpand(event)}/>
+
+              </View>
+
+              <View style={styles.condCont}>
+                          <Text style={styles.cond}> O </Text> 
+                          </View>
 
               </View>
 
@@ -75,7 +83,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems:'center',
         justifyContent: 'center',
-        marginTop: 5,
+        marginTop: 10,
     },
     title: {
         fontWeight: '600',
@@ -85,24 +93,42 @@ const styles = StyleSheet.create({
 
     },
     inputCont:{
-        alignItems:'center',
+        alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'row',
+        borderRadius: 5,
+        marginTop: 10,
+        marginRight: 15,
+        marginLeft: 10,
+        padding: 5,
         borderWidth: 2,
         borderColor: 'grey',
         backgroundColor: '#F8F9FB',
-        borderRadius: 5,
-        marginLeft: 10,
-        marginRight: 10,
+
         
 
     },
     input:{
-        width: width - 35,
+       // width: width - 35,
         fontSize: 16,
         paddingLeft: 5,
         color: '#C7C7CD',
         
-    }
+    },
+    cond: {
+        fontSize: 17,
+        textAlign: 'center',
+        fontWeight: '600',
+        color: 'white'
+    },
+    condCont: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 22,
+        height: 22,
+        borderRadius: 11,
+        backgroundColor: '#42D260'
+    },
 
 
  })
